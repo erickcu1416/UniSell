166a69df71f2d1e3571cfe1bd45fbf8f6e3c3640
Paraplejico69 <30453039+Paraplejico69@users.noreply.github.com>
2019-11-27 01:49:08 -0500

    Formulario para Vender

--- src/app/pages/sell/sell.page.ts
+++ src/app/pages/sell/sell.page.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MenuController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sell',
   templateUrl: './sell.page.html',
   styleUrls: ['./sell.page.scss'],
 })
+
 export class SellPage implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.menuCtrl.enable(true);
   }
 
-  constructor(private menuCtrl: MenuController) { }
+  constructor(private menuCtrl: MenuController, private router: Router) { }
 
   ngOnInit() {
     this.menuCtrl.enable(false);
   }
+
+    goToSell() {
+      this.router.navigateByUrl('form-add-product');
+    }
 
 }
